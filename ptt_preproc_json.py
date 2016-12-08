a4a5cb830c2a30ffd23349a3f5275204302ef0fd
Mosky <mosky.tw@gmail.com>
2016-12-08 21:28:59 +0800

    Use pathlib in the json

--- ptt_preproc_json.py
+++ ptt_preproc_json.py
@@ -2,6 +2,7 @@
 
 
 import json
+from pathlib import Path
 from os import scandir
 from os.path import (
     join as path_join,
@@ -14,24 +15,26 @@ import ptt_core
 l = ptt_core.l
 
 
-_PREPROCESSED_DIR_PATH = 'preprocessed'
+_PREPROCESSED_DIR_PATH = Path('preprocessed/')
+
+
+if not _PREPROCESSED_DIR_PATH.exists():
+    _PREPROCESSED_DIR_PATH.mkdir()
 
 
 def preprocess_to_json_file(html_path):
 
     l.info('Preprocessing {} ...'.format(html_path))
 
-    basename = to_basename(html_path)
-    root, ext = splitext(basename)
-    json_path = path_join(_PREPROCESSED_DIR_PATH, '{}.json'.format(root))
+    json_path = _PREPROCESSED_DIR_PATH / '{}.json'.format(html_path.stem)
 
-    if exists(json_path):
+    if json_path.exists():
         l.info('Existed and skip {}'.format(json_path))
         return
 
     # read, parse,
 
-    with open(html_path) as f:
+    with html_path.open() as f:
 
         try:
             parsed_article_d = ptt_core.parse_article_page(f)
@@ -43,20 +46,14 @@ def preprocess_to_json_file(html_path):
 
     # and transform
 
-    with ptt_core.mkdir_n_open(json_path, 'w') as f:
+    with json.path.open('w') as f:
         json.dump(parsed_article_d, f)
     l.info('Wrote into {}'.format(json_path))
 
 
-def preprocess_all(html_dir_path):
+def preprocess_all(html_dir_path_str):
 
-    for dir_entry in scandir(html_dir_path):
-
-        path = dir_entry.path
-
-        if 'index' in path:
-            continue
-
+    for path in Path(html_dir_path_str).glob('*index*'):
         preprocess_to_json_file(path)
 
 
