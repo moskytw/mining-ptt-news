14c1104d05c6ddbad8334266da6994339e1d8bfd
Mosky <mosky.tw@gmail.com>
2016-12-08 20:57:41 +0800

    Add ptt_sampling.py

---
+++ ptt_sampling.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+from pathlib import Path
+from random import shuffle
+from shutil import copy
+
+
+# configs
+
+N = 10000
+SAMPLED_DIR_PATH = Path('sampled/')
+
+# mkdir if doesn't exist
+
+if not SAMPLED_DIR_PATH.exists():
+    SAMPLED_DIR_PATH.mkdir(exist_ok=True)
+
+# sample and copy
+
+paths = [p for p in Path('preprocessed/').iterdir()]
+shuffle(paths)
+
+for p in paths[:N]:
+    copy(str(p), str(SAMPLED_DIR_PATH / p.name))
